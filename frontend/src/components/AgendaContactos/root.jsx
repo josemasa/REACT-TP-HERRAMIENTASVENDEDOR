b0a93035b5cc779d1527f3705e67dc1e945b0990
jose marcelo salas <utnfrt.josemarcelosalas@gmail.com>
2022-10-24 01:24:39 -0300

    contactos se ve bien

--- frontend/src/components/AgendaContactos/root.jsx
+++ frontend/src/components/AgendaContactos/root.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
 import { getContacts, createContact } from "./contacts";
-
+import styled from "styled-components";
 export async function action() {
   await createContact();
 }
@@ -14,52 +14,63 @@ export default function Root() {
   const { contacts } = useLoaderData();
   return (
     <>
-      <div id="sidebar">
-        <h1>Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <Form method="post">
-            <button type="submit">NUEVO</button>
-          </Form>
+      <MAIN>
+        <div className="conteineroot">
+          <div id="sidebar">
+            <h1>Contacts</h1>
+            <div>
+              <form id="search-form" role="search">
+                <input
+                  id="q"
+                  aria-label="Search contacts"
+                  placeholder="Search"
+                  type="search"
+                  name="q"
+                />
+                <div id="search-spinner" aria-hidden hidden={true} />
+                <div className="sr-only" aria-live="polite"></div>
+              </form>
+              <Form method="post">
+                <button type="submit">NUEVO</button>
+              </Form>
+            </div>
+            <nav>
+              {contacts.length ? (
+                <ul>
+                  {contacts.map((contact) => (
+                    <li key={contact.id}>
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite && <span>★</span>}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>No contacts</i>
+                </p>
+              )}
+            </nav>
+          </div>
+          <div id="detail">
+            <Outlet />
+          </div>
         </div>
-        <nav>
-          {contacts.length ? (
-            <ul>
-              {contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
-                    ) : (
-                      <i>No Name</i>
-                    )}{" "}
-                    {contact.favorite && <span>★</span>}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No contacts</i>
-            </p>
-          )}
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet />
-      </div>
+      </MAIN>
     </>
   );
 }
+
+const MAIN = styled.div`
+  .conteineroot {
+    display: flex;
+    flex-direction: row;
+  }
+`;
